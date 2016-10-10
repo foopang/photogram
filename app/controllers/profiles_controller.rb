a3da28e30d035d0c31d251282a4a92ee10f1e4af
Foo Pang <foo.pang@gmail.com>
2016-10-10 09:43:13 +0800

    WIP

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,28 @@
+class ProfilesController < ApplicationController
+  def show
+    @user = User.find_by(user_name: params[:user_name])
+    @posts = @user.posts.order('created_at DESC')
+  end
+
+  def edit
+    @user = User.find_by(user_name: params[:user_name])
+  end
+
+  def update
+    @user = User.find_by(user_name: params[:user_name])
+    if @user.update(profile_params)
+      flash[:success] = 'Your profile has been updated.'
+      redirect_to profile_path(@user.user_name)
+    else
+      @user.errors.full_messages
+      flash[:error] = @user.errors.full_messages
+      render :edit
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:user).permit(:avatar, :bio)
+  end
+end
